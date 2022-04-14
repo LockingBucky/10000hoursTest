e6181d5ddb658fa54807a52e5a4de28339583041
BUCKY <71341828+LockingBucky@users.noreply.github.com>
2022-04-14 16:59:51 +0900

    Add files via upload
    Code review and code improvement

--- js/10000hours.js
+++ js/10000hours.js
@@ -8,18 +8,18 @@ const loading = document.querySelector(".result_loading");
 
 function calculator(){
     const fieldValue = document.querySelector("#field_value");
-    let timeValue = document.querySelector("#time_value");
-    let timeValue_int = Number(timeValue.value);
+    const timeValue = document.querySelector("#time_value");
+    const timeValue_int = Number(timeValue.value);
 
     const fieldResult = document.querySelector(".field_result");
     const timeResult = document.querySelector(".time_result");
 
     if (fieldValue.value == "") {
-        alert("입력되지 않았습니다.");
+        alert("분야가 입력되지 않았습니다.");
         fieldValue.focus();
         return false;
     } else if (timeValue_int.value == "") {
-        alert("입력되지 않았습니다.");
+        alert("시간이 입력되지 않았습니다.");
         timeValue.focus();
         return false;
     } else if (timeValue_int > 24) {
@@ -30,11 +30,17 @@ function calculator(){
     loading.style.display = 'flex';
 
     setTimeout(function() {
-        loading.style.display = 'none';
-        result.style.display = 'flex';
         fieldResult.innerText = fieldValue.value;
         timeResult.innerText = parseInt((10000/timeValue_int), 10);
-    }, 1800);
+        loading.style.display = 'none';
+        result.style.display = 'flex';
+    }, 1800); 
+
+    // loading.style.display = 'none';
+    // result.style.display = 'flex';
+    // fieldResult.innerText = fieldValue.value;
+    // timeResult.innerText = parseInt((10000/timeValue_int), 10);
+    // ↑같이 하게되면 처리속도가 느린경우에 아무것도 없는 결과값이 노출이 될수있으므로 순서를 바뀔수있어서 이와 같이 안함.
 }
 
 function openModal(){
